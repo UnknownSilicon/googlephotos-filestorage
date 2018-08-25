0ded6fc2762b13c12724573e71d92789ee0022ad
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2018-08-25 16:21:01 -0400

    Fix checksums, add begining code for large files

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
+	private static final int MAX_SIZE = 16000000; // In pixels
+
 	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
 		Scanner scan = new Scanner(System.in);
 
@@ -26,13 +29,10 @@ public class Main {
 		if (s.equals("e")) {
 			File file = getFile(scan);
 
-			File outputFile = null;
-
 			System.out.println("Output File: ");
 			String fileStr = scan.nextLine();
-			outputFile = new File(fileStr);
 
-			encode(file, outputFile);
+			encode(file, fileStr);
 		} else if (s.equals("d")) {
 
 			File file = getFile(scan);
@@ -100,6 +100,17 @@ public class Main {
 			}
 		}
 
+		/* TODO: Figure out a better way to detect checksum | See below | The current "solution" will work for all files
+			Except for those with a perfect square image and the checksum ends in "255 255 255" or "FF FF FF"
+		*/
+		if ((data[lastIndex-1]&0xFF)==255 && (data[lastIndex]&0xFF)==255 && (data[lastIndex+1]&0xFF)==255) {
+			// If the last three bytes are 255, then you are in the right place
+			firstIndex-=3;
+		} else {
+			lastIndex = data.length;
+			firstIndex = lastIndex-19;
+		}
+
 		checksum = Arrays.copyOfRange(data, firstIndex+1, firstIndex+21);
 
 		byte[] newData = new byte[firstIndex+1];
@@ -109,7 +120,6 @@ public class Main {
 		}
 
 		// File should be encoded in data
-
 
 		FileOutputStream fos = new FileOutputStream(outputFile);
 
@@ -144,7 +154,7 @@ public class Main {
 		System.out.println("Decoded in: " + deltaTime + " seconds");
 	}
 
-	public static void encode(File file, File outputFile) throws IOException, NoSuchAlgorithmException {
+	public static void encode(File file, String outputFile) throws IOException, NoSuchAlgorithmException {
 		long startTime = System.nanoTime();
 		if (!file.exists()) {
 			System.out.println("File does not exist!");
@@ -161,21 +171,36 @@ public class Main {
 
 		int closestSquare = (int) Math.ceil(Math.sqrt(Long.divideUnsigned(size, 3L)));
 
+		int numberOfImages = (((closestSquare*closestSquare)-((closestSquare*closestSquare)%MAX_SIZE))/MAX_SIZE)+1;
+
 		ImageCreator ic = new ImageCreator(closestSquare, closestSquare);
 
-		int pixleNum = 0;
+		BufferedImage[] images = new BufferedImage[numberOfImages];
+
+		int currentImage = 1;
+		int pixelNum = 0;
 
 		byte[] byteArray = new byte[4096];
+		byte[] tempBytes = new byte[4096];
 		byte[] tempStorage = new byte[0];
 		int bytesCount = 0;
+		int previousBytesCount = 0;
+		long originalByteCount = 0;
 
-		while ((bytesCount = fis.read(byteArray)) != -1) {
+		while ((bytesCount = fis.read(tempBytes)) != -1) {
+
+			byteArray = tempBytes;
+
+			previousBytesCount = bytesCount;
+			originalByteCount += bytesCount;
+
+			byteArray = Arrays.copyOfRange(byteArray, 0, bytesCount); // Artificially shrink array
+
 			for (int i = 0; i < bytesCount; i += 3) {
-				int row = (pixleNum - (pixleNum % closestSquare)) / closestSquare;
-				int col = (pixleNum - closestSquare * row);
+				int row = (pixelNum - (pixelNum % closestSquare)) / closestSquare;
+				int col = (pixelNum - closestSquare * row);
 
 				try {
-
 					if (tempStorage.length == 1) {
 						ic.drawPixel(row, col, new Color(tempStorage[0] & 0xFF,
 								byteArray[i] & 0xFF, byteArray[i + 1] & 0xFF));
@@ -187,57 +212,87 @@ public class Main {
 						i-=2;
 						tempStorage = new byte[0];
 					} else {
-
 						ic.drawPixel(row, col, new Color(byteArray[i] & 0xFF,
 								byteArray[i + 1] & 0xFF, byteArray[i + 2] & 0xFF));
 					}
 
 				} catch (ArrayIndexOutOfBoundsException e) {
+					// This means that the number of bytes that was read is not divisible by 3, this will put it into multiple pixels
 
 					if (tempStorage.length != 0) {
 						System.out.println("Something broke");
 					}
 
 					tempStorage = Arrays.copyOfRange(byteArray, i, byteArray.length); // Copy up to the last two bytes
-					pixleNum--; // Counteract the ++
-
-					/*try {
-						ic.drawPixel(row, col, new Color(byteArray[i] & 0xFF,
-								byteArray[i + 1] & 0xFF, 0));
-					} catch (ArrayIndexOutOfBoundsException ee) {
-						try {
-							ic.drawPixel(row, col, new Color(byteArray[i] & 0xFF, 0, 0));
-						} catch (ArrayIndexOutOfBoundsException eee) {
-
-						}
-					}*/
+					pixelNum--; // Counteract the ++
 				}
-				pixleNum++;
+				pixelNum++;
 			}
+
+			if (pixelNum >= MAX_SIZE && currentImage < numberOfImages) {
+				currentImage++;
+
+				images[currentImage-1] = ic.getImage();
+
+				closestSquare = (int) Math.ceil(Math.sqrt(Long.divideUnsigned(size-MAX_SIZE, 3L)));
+
+				ic = new ImageCreator(closestSquare, closestSquare);
+
+				pixelNum = 0;
+			}
+
+		}
+
+		tempStorage = Arrays.copyOfRange(byteArray, Math.toIntExact(previousBytesCount - (originalByteCount % 3L)), previousBytesCount);
+
+		if (tempStorage.length >= 3) {
+			System.out.println("Something else broke");
 		}
 
         for (int i=0; i<checksum.length; i+=3) {
-	        int row = (pixleNum - (pixleNum % closestSquare)) / closestSquare;
-	        int col = (pixleNum - closestSquare * row);
+	        int row = (pixelNum - (pixelNum % closestSquare)) / closestSquare;
+	        int col = (pixelNum - closestSquare * row);
 
-            try {
-                ic.drawPixel(row, col, new Color(checksum[i] & 0xFF, checksum[i + 1] & 0xFF, checksum[i + 2] & 0xFF));
-            } catch (ArrayIndexOutOfBoundsException e) {
-                try {
-                    ic.drawPixel(row, col, new Color(checksum[i] & 0xFF, checksum[i + 1] & 0xFF, 0));
-                } catch (ArrayIndexOutOfBoundsException ee) {
-                	try {
-						ic.drawPixel(row, col, new Color(checksum[i] & 0xFF, 0, 0));
-					} catch (ArrayIndexOutOfBoundsException eee) {
+	        if (tempStorage.length == 1) {
+				ic.drawPixel(row, col, new Color(tempStorage[0] & 0xFF, checksum[i] & 0xFF, checksum[i + 1] & 0xFF));
+				tempStorage = new byte[0];
+				i--;
+			} else if (tempStorage.length == 2) {
+				ic.drawPixel(row, col, new Color(tempStorage[0] & 0xFF, tempStorage[1] & 0xFF, checksum[i] & 0xFF));
+				tempStorage = new byte[0];
+				i-=2;
+			} else {
+				try {
+					ic.drawPixel(row, col, new Color(checksum[i] & 0xFF, checksum[i + 1] & 0xFF, checksum[i + 2] & 0xFF));
+				} catch (ArrayIndexOutOfBoundsException e) {
+					try {
+						ic.drawPixel(row, col, new Color(checksum[i] & 0xFF, checksum[i + 1] & 0xFF, 0));
+					} catch (ArrayIndexOutOfBoundsException ee) {
+						try {
+							ic.drawPixel(row, col, new Color(checksum[i] & 0xFF, 0, 0));
+						} catch (ArrayIndexOutOfBoundsException eee) {
 
+						}
 					}
-                }
-            }
-            pixleNum++;
+				}
+			}
+            pixelNum++;
         }
 
-		BufferedImage image = ic.getImage();
-		ImageIO.write(image, "png", outputFile);
+		int row = (pixelNum - (pixelNum % closestSquare)) / closestSquare;
+		int col = (pixelNum - closestSquare * row);
+
+		// TODO: Figure out a better way to detect the checksum
+		if (row < closestSquare && col < closestSquare) { // If there is still room left
+			ic.drawPixel(row, col, new Color(255, 255, 255));
+		}
+
+		images[currentImage - 1] = ic.getImage();
+
+        int imgNum = 0;
+        for (BufferedImage image : images) {
+        	ImageIO.write(image, "png", new File(outputFile + "-" + imgNum + ".png"));
+		}
 
 		long endTime = System.nanoTime();
 
