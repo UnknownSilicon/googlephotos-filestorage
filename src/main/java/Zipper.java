1942c5015316fb5f735c439a3e9f657c3dde7a19
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2018-09-04 14:01:59 -0400

    Add large file encoding and fix checksums

--- src/main/java/Zipper.java
+++ src/main/java/Zipper.java
@@ -1,5 +1,6 @@
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.ZipParameters;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,15 +16,15 @@ public class Zipper {
 	public String zip(File source) throws ZipException, IOException {
 		Path tempDir = Files.createTempDirectory("gfs");
 
-		String outputFile = tempDir.toString() + "/" + source.getName() + ".zip";
+		String outputDir = tempDir.toString();
 
-		ZipFile file = new ZipFile(outputFile);
+		ZipFile file = new ZipFile(outputDir + "/" + source.getName() + ".zip");
 
-		long maxBytes = Main.MAX_SIZE*3;
+		long maxBytes = Main.MAX_SIZE*3-20;
 
-		file.createZipFile(source, null, true, maxBytes);
+		file.createZipFile(source, new ZipParameters(), true, maxBytes);
 
-		return outputFile;
+		return outputDir;
 	}
 
 	public void unzip(String sourceDir) throws ZipException {
